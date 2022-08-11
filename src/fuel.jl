a8f63ac9bd0e2c99338b9fab5e39e2714395f7bb
vavrines <tianbaixiao@gmail.com>
2022-08-11 09:27:44 +0200

    Change income units & update figs

--- src/fuel.jl
+++ src/fuel.jl
@@ -3,6 +3,7 @@ using CSV, DataFrames, GLM, Plots
 # read data
 cd(@__DIR__)
 df = DataFrame(CSV.File("../data/fuel2001.csv"))
+df.IncomeN = df.Income ./ 1000
 
 # visualize data
 scatter(df.Pop, df.FuelC, legend = :none)
@@ -11,14 +12,14 @@ savefig("../fig/pop-fuelc.pdf")
 scatter(df.Miles, df.FuelC, legend = :none)
 savefig("../fig/miles-fuelc.pdf")
 
-scatter(df.Income, df.FuelC, legend = :none)
+scatter(df.IncomeN, df.FuelC, legend = :none)
 savefig("../fig/income-fuelc.pdf")
 
 scatter(df.Tax, df.FuelC, legend = :none)
 savefig("../fig/tax-fuelc.pdf")
 
 # process data
-Fuel = @. 1000 * df.FuelC / df.Pop 
+Fuel = @. 1000 * df.FuelC / df.Pop
 Dlic = @. 1000 * df.Drivers / df.Pop
 Logmiles = log2.(df.Miles)
 
@@ -32,12 +33,11 @@ savefig("../fig/tax-fuel.pdf")
 scatter(df.Dlic, df.Fuel, legend = :none)
 savefig("../fig/dlic-fuel.pdf")
 
-scatter(df.Income, df.Fuel, legend = :none)
+scatter(df.IncomeN, df.Fuel, legend = :none)
 savefig("../fig/income-fuel.pdf")
 
 scatter(df.Logmiles, df.Fuel, legend = :none)
 savefig("../fig/logmiles-fuel.pdf")
 
 # fit model
-res = fit(LinearModel, @formula(Fuel ~ Tax + Dlic + Income + Logmiles), df)
-
+res = fit(LinearModel, @formula(Fuel ~ Tax + Dlic + IncomeN + Logmiles), df)
